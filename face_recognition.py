7570b47670e21680da57668c94e67384b9d531b9
Hossam Hassan <hossam.hassan3002@gmail.com>
2025-09-16 03:44:53 +0300

    n

--- face_recognition.py
+++ face_recognition.py
@@ -2,6 +2,7 @@ import cv2 as cv
 import numpy as np 
 import os
 import random
+
 print("HI")
 # Load Haar Cascade
 haar_cascade = cv.CascadeClassifier('haarcascade_frontalface_alt.xml')
@@ -39,5 +40,7 @@ for (x, y, w, h) in faces_rect:
     cv.rectangle(img, (x, y), (x+w, y+h), (0,255,0), 2)
 
 cv.imshow('image', img)
-cv.waitKey(0)
-cv.destroyAllWindows()
+if cv.waitKey(0) and   0xFF == ord('q'):
+    cv.destroyAllWindows()
+    exit()
+
